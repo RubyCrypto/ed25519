5456b8958be55cd44ab0c1acf57361490f3aec00
Tony Arcieri <tony.arcieri@gmail.com>
2012-10-01 21:00:49 -0700

    Initial verification algorithm binding
    I still have the sneaking suspicion there's something wrong with this
    code. I was, at one point, getting segvs, but now attempts to reproduce
    them, even under the scrutiny of valgrind, are failing.
    
    I'm committing it for now in hopes of reproducing the issue on Travis.

--- ext/red25519/red25519_engine.c
+++ ext/red25519/red25519_engine.c
@@ -5,6 +5,8 @@ static VALUE mEd25519 = Qnil;
 static VALUE mEd25519_Engine = Qnil;
 
 static VALUE Ed25519_Engine_create_keypair(VALUE self, VALUE seed);
+static VALUE Ed25519_Engine_sign(VALUE self, VALUE signing_key, VALUE msg);
+static VALUE Ed25519_Engine_verify(VALUE self, VALUE verify_key, VALUE signature, VALUE msg);
 
 void Init_red25519_engine()
 {
@@ -12,6 +14,8 @@ void Init_red25519_engine()
     mEd25519_Engine = rb_define_module_under(mEd25519, "Engine");
 
     rb_define_singleton_method(mEd25519_Engine, "create_keypair", Ed25519_Engine_create_keypair, 1);
+    rb_define_singleton_method(mEd25519_Engine, "sign", Ed25519_Engine_sign, 2);
+    rb_define_singleton_method(mEd25519_Engine, "verify", Ed25519_Engine_verify, 3);
 }
 
 static VALUE Ed25519_Engine_create_keypair(VALUE self, VALUE seed)
@@ -29,4 +33,48 @@ static VALUE Ed25519_Engine_create_keypair(VALUE self, VALUE seed)
         rb_str_new(verify_key, PUBLICKEYBYTES),
         rb_str_new(signing_key, SECRETKEYBYTES)
     );
+}
+
+static VALUE Ed25519_Engine_sign(VALUE self, VALUE signing_key, VALUE msg)
+{
+    unsigned char *sig_and_msg;
+    unsigned long long sig_and_msg_len;
+    VALUE result;
+
+    if(RSTRING_LEN(signing_key) != SECRETKEYBYTES)
+        rb_raise(rb_eArgError, "private signing keys must be %d bytes", SECRETKEYBYTES);
+
+    sig_and_msg = (unsigned char *)xmalloc(SIGNATUREBYTES + RSTRING_LEN(msg));
+    crypto_sign(sig_and_msg, &sig_and_msg_len, RSTRING_PTR(msg), RSTRING_LEN(msg), RSTRING_PTR(signing_key));
+    result = rb_str_new(sig_and_msg, SIGNATUREBYTES);
+    free(sig_and_msg);
+
+    return result;
+}
+
+static VALUE Ed25519_Engine_verify(VALUE self, VALUE verify_key, VALUE signature, VALUE msg)
+{
+    unsigned char *sig_and_msg;
+    unsigned long long msg_len, sig_and_msg_len;
+    int result;
+
+    if(RSTRING_LEN(verify_key) != PUBLICKEYBYTES)
+      rb_raise(rb_eArgError, "public verify keys must be %d bytes", PUBLICKEYBYTES);
+
+    if(RSTRING_LEN(signature) != SIGNATUREBYTES)
+      rb_raise(rb_eArgError, "public verify keys must be %d bytes", PUBLICKEYBYTES);
+
+    sig_and_msg_len = SIGNATUREBYTES + RSTRING_LEN(msg);
+    sig_and_msg = (unsigned char *)xmalloc(sig_and_msg_len);
+    memcpy(sig_and_msg, RSTRING_PTR(signature), SIGNATUREBYTES);
+    memcpy(sig_and_msg + SIGNATUREBYTES, RSTRING_PTR(msg), RSTRING_LEN(msg));
+
+    result = crypto_sign_open(
+        RSTRING_PTR(msg), &msg_len,
+        sig_and_msg, sig_and_msg_len,
+        RSTRING_PTR(verify_key));
+
+    free(sig_and_msg);
+
+    return result == 0 ? Qtrue : Qfalse;
 }
--- spec/red25519/engine_spec.rb
+++ spec/red25519/engine_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Ed25519::Engine do
   let(:seed_length) { Ed25519::SECRET_KEY_BYTES / 2 }
+  let(:message)     { 'foobar' }
 
   it "generates keypairs" do
     ary = Ed25519::Engine.create_keypair("A" * seed_length)
@@ -19,5 +20,14 @@ describe Ed25519::Engine do
   it "raises ArgumentError if the seed is not #{Ed25519::SECRET_KEY_BYTES / 2} bytes long" do
     expect { Ed25519::Engine.create_keypair("A" * (seed_length - 1)) }.to raise_exception ArgumentError
     expect { Ed25519::Engine.create_keypair("A" * (seed_length + 1)) }.to raise_exception ArgumentError
+  end
+
+  it "signs and verifies messages" do
+    verify_key, signing_key = Ed25519::Engine.create_keypair("A" * seed_length)
+    signature = Ed25519::Engine.sign(signing_key, message)
+    Ed25519::Engine.verify(verify_key, signature, message).should be_true
+
+    bad_signature = signature[0...63] + "X"
+    Ed25519::Engine.verify(verify_key, bad_signature, message).should be_false
   end
 end
