441e378ac9d44146914e2761110a63d4b2d88cba
Tony Arcieri <bascule@gmail.com>
2025-05-19 06:53:10 -0600

    Fix CI: disable Metrics/BlockLength for test vectors

--- spec/ed25519/test_vectors_spec.rb
+++ spec/ed25519/test_vectors_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require "spec_helper"
 
 RSpec.describe Ed25519::SigningKey do
@@ -157,3 +159,5 @@ SIG
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
